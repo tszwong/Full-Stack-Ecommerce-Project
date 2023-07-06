ac82c5cc259a9b891b277a7b4a0e4d7b61415e98
tszwong <wongt@bu.edu>
2023-07-05 20:46:29 -0400

    Angular front end product list

--- 02-backend/spring-boot-ecommerce/src/main/java/com/tszwong/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tszwong/ecommerce/dao/ProductCategoryRepository.java
@@ -4,8 +4,10 @@ import com.tszwong.ecommerce.entity.Product;
 import com.tszwong.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // @RepositoryRestResource(collectionResourceRel={Name of JSON Entry}, path={reference of path})
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- 02-backend/spring-boot-ecommerce/src/main/java/com/tszwong/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tszwong/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,10 @@ package com.tszwong.ecommerce.dao;
 
 import com.tszwong.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// @CrossOrigin accepts calls from web browser scripts from this origin
+// Origin = protocl + hostname + port
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
