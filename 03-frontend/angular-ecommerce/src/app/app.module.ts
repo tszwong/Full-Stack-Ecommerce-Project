2a8fc86fe173485cb2860bde1134dae7d877e1b0
tszwong <wongt@bu.edu>
2023-08-09 17:15:22 -0400

    product master-detail view done

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { ProductService } from './services/product.service';
 
 import { Routes, RouterModule } from "@angular/router";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component'
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component'
 
 // the routes are top down prioritized, start from specific to general
 const routes: Routes = [
+  {path: "products/:id", component: ProductDetailsComponent},
   {path: "search/:keyword", component: ProductListComponent},
   {path: "category/:id", component: ProductListComponent}, // when path matches it creates a new instance of component
   {path: "category", component: ProductListComponent},
@@ -25,7 +27,8 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     ProductCategoryMenuComponent,
-    SearchComponent
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -12,7 +12,14 @@ export class ProductService {
   private baseUrl = "http://localhost:8080/api/products";
   private categoryUrl = "http://localhost:8080/api/product-category";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProduct(theProductId: number): Observable<Product> {
+    // need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id
