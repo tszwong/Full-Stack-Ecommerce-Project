6c28508bd60ffffdee97254659f853cfabafed94
tszwong <wongt@bu.edu>
2023-08-30 02:35:40 -0400

    security order history secure backend done

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tszwong/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.tszwong.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //protect endpoint /api/orders
+        http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer()
+                .jwt();
+
+        //add CORS filters
+        http.cors();
+
+        //add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new
+                HeaderContentNegotiationStrategy());
+
+        //force a non-empty response body for 401's to make the response more friendly
+
+        Okta.configureResourceServer401ResponseBody(http);
+
+        // disable CSRF since we are not using Cookies for session tracking
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+}
